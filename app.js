afa62248d7fc37973764c211ccd7ea90f2ee3815
Ana Luiza <analuiza.daso@gmail.com>
2025-03-24 20:28:21 -0300

    Adicionar formulário e função de recuperação de senha.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ let campoEmail = document.getElementById('email');
 let campoSenha = document.getElementById('senha');
 var buttonEntrar = document.getElementById('buttonEntrar');
 var textoEmailSenhaInvalido = document.getElementById('textoEmailSenhaInvalido');
+var recuperarSenha = document.getElementById('recuperarSenha');
+var formRecuperacao = document.getElementById('formRecuperacao');
+var cancelarRecuperacao = document.getElementById('cancelarRecuperacao');
 
-buttonEntrar.addEventListener ('click', function(){
+// Função para validação de entrada: E-mail e senha
+buttonEntrar.addEventListener('click', function() {
     var valorEmail = campoEmail.value;
     var valorSenha = campoSenha.value;
 
@@ -13,16 +17,30 @@ buttonEntrar.addEventListener ('click', function(){
         if (valorSenha.length > 0) {
             console.log("E-mail e senha preenchidos corretamente");
             textoEmailSenhaInvalido.style.display = 'none';
-        }else {
+        } else {
             ErroEmailSenhaInvalido();
         }
-    }else {
+    } else {
         ErroEmailSenhaInvalido();
     }
-})
+});
 
+// Função para mensagem de erro dos campos de e-mail e senha
 function ErroEmailSenhaInvalido() {
     const EmailSenhaInvalido = "E-mail ou senha inválidos.";
     textoEmailSenhaInvalido.textContent = EmailSenhaInvalido;
     textoEmailSenhaInvalido.style.display = 'block';
 }
+
+// Função para mostrar o formulário de recuperação de senha
+recuperarSenha.addEventListener('click', function(event) {
+    event.preventDefault();
+    formRecuperacao.style.display = 'block';
+    formLogin.style.display = 'none';
+});
+
+// Função para ocultar o formulário de recuperação de senha
+cancelarRecuperacao.addEventListener('click', function() {
+    formRecuperacao.style.display = 'none';
+    formLogin.style.display = 'block';
+});
